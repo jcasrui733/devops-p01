f4085476a9a790fb89268fd9b323d283e7380485
jcasrui733 <jcasrui733@fp.iesromerovargas.com>
2026-01-14 14:15:50 +0100

    añado app a python

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # app.py
 """ docstring del modulo"""
-
+# añadir algo
 import json
 
 def suma(a: int, b: int) -> int:
     """ docstring función"""
     x = "no se usa"
+    y = "a"
     return a+ b
